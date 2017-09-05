9dfbc63e2e2a20d8fc91777009e2d8d0faf0ba12
linxin <zenglinxin@163.com>
2017-09-05 15:03:53 +0800

    增加Ribbon负载均衡

---
+++ RibbonClient/src/main/java/hello/UserApplication.java
@@ -0,0 +1,50 @@
+
+package hello;
+
+/**
+ * Created by @author linxin on 05/09/2017.  <br>
+ */
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@RestController
+@RibbonClient(name = "say-hello", configuration = SayHelloConfiguration.class)
+public class UserApplication {
+
+    @LoadBalanced
+    @Bean
+    RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    private static Logger log = LoggerFactory.getLogger(UserApplication.class);
+
+    @RequestMapping("/hi")
+    public String hi(@RequestParam(value="name", defaultValue="Artaban") String name) {
+
+//        String greeting = this.restTemplate.getForObject("http://localhost:8801/greeting", String.class);
+        String greeting = this.restTemplate.getForObject("http://say-hello/greeting", String.class);
+        log.info("hi "+greeting+" "+name);
+        return String.format("%s, %s!", greeting, name);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
