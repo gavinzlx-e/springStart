532165dd4890f34f33d684620b37cff5725de75d
linxin <zenglinxin@163.com>
2017-09-09 19:38:46 +0800

    增加 eureka 配置！

---
+++ EurekaClient/src/main/java/hello/PriceService.java
@@ -0,0 +1,17 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by @author linxin on 09/09/2017br>
+ */
+@RestController
+public class PriceService {
+
+    @RequestMapping(value = "/getPrice" ,method= RequestMethod.GET)
+    public String getPrice(){
+        return "100";
+    }
+}
